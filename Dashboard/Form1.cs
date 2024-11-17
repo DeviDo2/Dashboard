85a6f1c35bce11639653ad20f12f62fb438f9040
DeVera2 <rjdeverayt2@gmail.com>
2024-11-17 18:37:35 +0800

    Add project files.

---
+++ Dashboard/Form1.cs
@@ -0,0 +1,80 @@
+namespace Dashboard
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void home_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inventory_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void itemGrp_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salesOrders_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salesReturns_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void purchaseOrders_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reports_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void salesAct_groupBox_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void search_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void profile_pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void topItemName_label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
